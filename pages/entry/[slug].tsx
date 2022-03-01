0b6afb47f4f3679b8a5e1604c18903a54c90912f
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-03-01 15:12:05 -0300

    Plant detail: Add sidebar sections

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -1,13 +1,17 @@
 import { GetStaticProps, InferGetStaticPropsType, GetStaticPaths } from 'next'
-import { getPlant, getPlantList } from '@api'
+import Link from 'next/link'
+import { getPlant, getPlantList, getCategoryList } from '@api'
 import { Layout } from '@components/Layout'
 import { Typography } from '@ui/Typography'
 import { Grid } from '@ui/Grid'
 import { RichText } from '@components/RichText'
 import { AuthorCard } from '@components/AuthorCard'
+import { PlantEntryInline } from '@components/PlantCollection'
 
 type PlantEntryPageProps = {
   plant: Plant
+  otherEntries: Plant[]
+  categories: Category[]
 }
 
 export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
@@ -24,9 +28,16 @@ export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
   try {
     const plant = await getPlant(slug)
 
+    const otherEntries = await getPlantList({
+      limit: 5,
+    })
+    const categories = await getCategoryList({ limit: 10 })
+
     return {
       props: {
         plant,
+        otherEntries,
+        categories,
       },
     }
   } catch (e) {
@@ -60,6 +71,8 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
 export default function PlantEntryPage({
   plant,
+  otherEntries,
+  categories,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <Layout>
@@ -80,11 +93,27 @@ export default function PlantEntryPage({
             <Typography variant="h5" component="h3" className="mb-4">
               Recent posts
             </Typography>
+            {otherEntries.map((plantEntry) => (
+              <article className="mb-4" key={plantEntry.id}>
+                <PlantEntryInline {...plantEntry} />
+              </article>
+            ))}
           </section>
           <section className="mt-10">
             <Typography variant="h5" component="h3" className="mb-4">
               Categories
             </Typography>
+            <ul className="list">
+              {categories.map((category) => (
+                <li key={category.id}>
+                  <Link passHref href={`/category/${category.slug}`}>
+                    <Typography component="a" variant="h6">
+                      {category.title}
+                    </Typography>
+                  </Link>
+                </li>
+              ))}
+            </ul>
           </section>
         </Grid>
       </Grid>
