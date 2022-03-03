03d1ffc85c3aad331ef397aa5acd28cfd68db55e
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-03-03 14:10:36 -0300

    TopStories: Add index and author page

---
+++ pages/top-stories/index.tsx
@@ -0,0 +1,40 @@
+import Error from 'next/error'
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
+import { getAuthorList } from '@api'
+
+export const getServerSideProps: GetServerSideProps<{
+  error: number
+}> = async () => {
+  try {
+    const authors = await getAuthorList({ limit: 10 })
+
+    if (authors.length > 0) {
+      const firstAuthor = authors[0].handle
+
+      return {
+        redirect: {
+          destination: `/top-stories/${firstAuthor}`,
+          permanent: false,
+        },
+      }
+    }
+
+    return {
+      props: {
+        error: 412,
+      },
+    }
+  } catch (e) {
+    return {
+      props: {
+        error: 400,
+      },
+    }
+  }
+}
+
+export default function TopStoriesIndex({
+  error,
+}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  return <Error statusCode={error} />
+}
