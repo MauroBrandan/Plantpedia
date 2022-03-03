34cdb216fcc276918e67c4e97093a24535f7dd16
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-03-03 14:26:13 -0300

    Router: use shallow nav in Top Stories

--- pages/top-stories/[author].tsx
+++ pages/top-stories/[author].tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { useState, useEffect } from 'react'
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next'
 
@@ -13,7 +14,6 @@ import { IGetPlantListByAuthorQueryVariables } from '@api/generated/graphql'
 
 type TopStoriesPageProps = {
   authors: Author[]
-  currentAuthor: Author['handle']
   status: 'error' | 'sucess'
 }
 
@@ -43,7 +43,6 @@ export const getServerSideProps: GetServerSideProps<
     return {
       props: {
         authors,
-        currentAuthor: authorHandle,
         status: 'sucess',
       },
     }
@@ -51,7 +50,6 @@ export const getServerSideProps: GetServerSideProps<
     return {
       props: {
         authors: [],
-        currentAuthor: authorHandle,
         status: 'error',
       },
     }
@@ -60,12 +58,16 @@ export const getServerSideProps: GetServerSideProps<
 
 export default function TopStories({
   authors,
-  currentAuthor,
   status,
 }: InferGetServerSidePropsType<typeof getServerSideProps>) {
-  const [currentTab, setCurrentTab] = useState(currentAuthor)
+  const router = useRouter()
+  const currentAuthor = router.query.author
 
-  if (authors.length === 0 || status === 'error') {
+  if (
+    typeof currentAuthor !== 'string' ||
+    authors.length === 0 ||
+    status === 'error'
+  ) {
     return (
       <Layout>
         <main className="pt-10 px-6">
@@ -98,8 +100,12 @@ export default function TopStories({
         </div>
         <VerticalTabs
           tabs={tabs}
-          currentTab={currentTab}
-          onTabChange={(_, newValue) => setCurrentTab(newValue)}
+          currentTab={currentAuthor}
+          onTabChange={(_, newValue) => {
+            router.push(`/top-stories/${newValue}`, undefined, {
+              shallow: true,
+            })
+          }}
         />
       </main>
     </Layout>
