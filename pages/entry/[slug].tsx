8821f5e212b1de628506bbb0e3065cbf5c07fdd5
Mauro Brandan <maurobrandan.mb@gmail.com>
2022-03-04 15:17:25 -0300

    Preview Mode: Set it up

--- pages/entry/[slug].tsx
+++ pages/entry/[slug].tsx
@@ -17,6 +17,7 @@ type PlantEntryPageProps = {
 
 export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
   params,
+  preview,
 }) => {
   const slug = params?.slug
 
@@ -27,7 +28,7 @@ export const getStaticProps: GetStaticProps<PlantEntryPageProps> = async ({
   }
 
   try {
-    const plant = await getPlant(slug)
+    const plant = await getPlant(slug, preview)
 
     const otherEntries = await getPlantList({
       limit: 5,
